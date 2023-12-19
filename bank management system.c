0c398377e56c5b4bc9444b17cb1a437e95b7a081
suchitbhayani <149994411+suchitbhayani@users.noreply.github.com>
2023-12-19 13:23:41 -0800

    Add files via upload
    created basic ui functionality
    
    - create struct for account next

--- bank management system.c
+++ bank management system.c
@@ -1,7 +1,39 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdbool.h> 
 
 int main() {
-    printf("Hello world!");
+    bool keepRunning = true;
+    char mainScreenInput;
+
+    printf("Welcome to Bhayani Banking!\n");
+
+    while (keepRunning) {
+        printf("\nPlease select an action: \n");
+
+        printf("(1) - Create account\n");
+        printf("(2) - Login\n");
+        printf("(X) - Exit\n");
+        scanf(" %c", &mainScreenInput);
+        
+        switch(mainScreenInput) {
+        case '1' :
+            printf("Creating account!\n");
+            break;
+        case '2' :
+            printf("Logging in!\n");
+            break;
+        case 'X' :
+            printf("Thank you for banking with us!\n");
+            keepRunning = false;
+            break;
+        default :
+            printf("Invalid input!\n");
+            break;
+
+        }
+
+    }
+
 
     return 0;
 
