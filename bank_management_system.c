11f821955c8e4be1be0e63cf82bd920370bf6d32
suchitbhayani <149994411+suchitbhayani@users.noreply.github.com>
2023-12-20 20:26:51 -0800

    Add files via upload
    - created function accountExistsUserPass which checks if a given username and password matches w a bank account
    - created function login which inputs user for a username and password, and calls accountExistsUserPass to see if that user and pass exist

--- bank_management_system.c
+++ bank_management_system.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h> 
+#include <string.h>
 
 struct BankAccount {
     char username[20];
@@ -8,19 +9,54 @@ struct BankAccount {
 };
 
 struct BankAccount accounts[50];
+const size_t accountsSize = sizeof(accounts) / sizeof(accounts[0]);
 int nextFreeAccount = 0;
 
+
 void createAccount() {
-    if (nextFreeAccount < 50) {
-        printf("\nEnter username: \n");
+    if (nextFreeAccount < accountsSize) {
+        printf("\nEnter new username: \n");
         scanf("%s", accounts[nextFreeAccount].username);
-        printf("\nEnter password: \n");
+        printf("\nEnter new password: \n");
         scanf("%s", accounts[nextFreeAccount].password);
         accounts[nextFreeAccount].balance = 0.00;
         printf("\nAccount created. Balance is currently $%d\n\n", accounts[nextFreeAccount].balance);
+
+        printf("%s", accounts[nextFreeAccount].username);
+
         nextFreeAccount++;
+
     } else {
         printf("Too many accounts created!");
+    }
+}
+
+
+bool accountExistsUserPass(char* username, char* password) {   
+    for (int i = 0; i < accountsSize; i++) {
+        if (strcmp(accounts[i].username, username) == 0 && strcmp(accounts[i].password, password) == 0) {
+            return true;
+        }
+    }
+
+    return false;
+    
+}
+
+
+void login() {
+    char enteredUser[20];
+    char enteredPassword[20];
+
+    printf("\nEnter username:\n");
+    scanf("%s", enteredUser);
+    printf("\nEnter password\n");
+    scanf("%s", enteredPassword);
+
+    if (accountExistsUserPass(enteredUser, enteredPassword)) {
+        // launch login menu
+    } else {
+        printf("\nSorry, account does not exist!\n");
     }
 }
 
@@ -31,7 +67,7 @@ int main() {
     printf("Welcome to Bhayani Banking!\n");
 
     while (keepRunning) {
-        printf("\nPlease select an action: \n");
+        printf("\nPlease select an action: \n\n");
 
         printf("(1) - Create account\n");
         printf("(2) - Login\n");
@@ -43,6 +79,7 @@ int main() {
             createAccount();
             break;
         case '2' :
+            login();
             break;
         case 'X' :
             printf("Thank you for banking with us!\n");
