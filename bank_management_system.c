80b389b6a6bf9bddd0141276bc6ac6dae64c3298
suchitbhayani <149994411+suchitbhayani@users.noreply.github.com>
2023-12-20 18:14:28 -0800

    Add files via upload
    - created BankAccount struct with username, password, balance
    - created createAccount() which creates bank account structs, stores them in an array

--- bank_management_system.c
+++ bank_management_system.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
 #include <stdbool.h> 
 
+struct BankAccount {
+    char username[20];
+    char password[20];
+    double balance;
+};
+
+struct BankAccount accounts[50];
+int nextFreeAccount = 0;
+
+void createAccount() {
+    if (nextFreeAccount < 50) {
+        printf("\nEnter username: \n");
+        scanf("%s", accounts[nextFreeAccount].username);
+        printf("\nEnter password: \n");
+        scanf("%s", accounts[nextFreeAccount].password);
+        accounts[nextFreeAccount].balance = 0.00;
+        printf("\nAccount created. Balance is currently $%d\n\n", accounts[nextFreeAccount].balance);
+        nextFreeAccount++;
+    } else {
+        printf("Too many accounts created!");
+    }
+}
+
 int main() {
     bool keepRunning = true;
     char mainScreenInput;
@@ -12,15 +35,14 @@ int main() {
 
         printf("(1) - Create account\n");
         printf("(2) - Login\n");
-        printf("(X) - Exit\n");
+        printf("(X) - Exit\n\n");
         scanf(" %c", &mainScreenInput);
         
         switch(mainScreenInput) {
         case '1' :
-            printf("Creating account!\n");
+            createAccount();
             break;
         case '2' :
-            printf("Logging in!\n");
             break;
         case 'X' :
             printf("Thank you for banking with us!\n");
@@ -33,7 +55,6 @@ int main() {
         }
 
     }
-
 
     return 0;
 
